bd8fc166bd57aa9750747c2cf9cb33d4c3ff8129
오수빈 <catherine621@naver.com>
2022-11-19 21:35:03 +0900

    PageHeaderFix

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader/PageHeader';
 
 import css from './Login.module.scss';
 
 function Login() {
-  return <div className={css.container}></div>;
+  return (
+    <div className={css.container}>
+      <PageHeader />
+    </div>
+  );
 }
 
 export default Login;
