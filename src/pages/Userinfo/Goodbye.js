a4b3ac178b665abef8b5d441a52cc697c8dd5a39
DawonOh <dawonohh@gmail.com>
2022-11-24 08:58:47 +0900

    Add: 탈퇴,비밀번호 변경 페이지 추가

--- src/pages/Userinfo/Goodbye.js
+++ src/pages/Userinfo/Goodbye.js
@@ -6,7 +6,6 @@ import Footer from '../../components/Footer/Footer';
 import ChangeInfo from '../../components/UserInfo/ChangeInfo/ChangeInfo';
 
 const Goodbye = () => {
-  let isPass = localStorage.getItem('changeInfoPassCode');
   return (
     <Fragment>
       <PageHeader />
@@ -14,8 +13,40 @@ const Goodbye = () => {
         <div className={css.userinfoWrap}>
           <MypageSidebar />
           <div className={css.userinfoMainWrap}>
-            <h1 className={css.userInfoTitle}>회원탈퇴</h1>
-            <div className={css.userInfoMain}></div>
+            <h1 className={css.userInfoTitle}>회원 탈퇴</h1>
+            <div className={css.userInfoMain}>
+              <h2>
+                회원님의 비밀번호를 입력하시고 [탈퇴] 버튼을 클릭해주세요.
+              </h2>
+              <table>
+                <tbody>
+                  <tr>
+                    <th>비밀번호</th>
+                    <td>
+                      <input type="text" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>휴대폰 번호</th>
+                    <td>
+                      <input type="text" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>인증번호</th>
+                    <td>
+                      <input type="text" />
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className={css.changeInfoBtns}>
+                <button className={css.cancleBtn}>취소</button>
+                <button className={css.disgoodbytJegaBoxBtn} disabled>
+                  탈퇴
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
