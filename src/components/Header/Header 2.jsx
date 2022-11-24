8619a89328e8ba3283b73140b2a8f609b18eb08a
RBGonion <yopi27@naver.com>
2022-11-25 08:34:32 +0900

    merge 후 수정

--- src/components/Header/Header 2.jsx
+++ src/components/Header/Header 2.jsx
@@ -1,23 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { FiMenu } from 'react-icons/fi';
 import { BiSearch } from 'react-icons/bi';
 import { FaCalendarAlt } from 'react-icons/fa';
 import { AiOutlineUser } from 'react-icons/ai';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import '../../components/ResetSubin.scss';
-import css from './PageHeader.module.scss';
+import css from './Header.module.scss';
 import { Link } from 'react-router-dom';
-import Dropdown from '../Header/Dropdown/Dropdown';
+import mainImg from '../../img/jegabox.jpg';
+import Dropdown from './Dropdown/Dropdown';
 import { useState } from 'react';
-import GlassDropdown from '../Header/GlassDropDown/GlassDropdown';
-import UnderMenu from '../PageHeader/UnderMenu/UnderMenu';
-import NonMember from '../Header/NonMember/NonMember';
-import { Member } from '../Header/Member/Member';
-import LoginModal from '../../components/Login/LoginModal';
-import { useNavigate } from 'react-router-dom';
-
-function PageHeader() {
-  const navigate = useNavigate();
+import GlassDropdown from './GlassDropDown/GlassDropdown';
+import UnderMenu from './UnderMenu/UnderMenu';
+import NonMember from './NonMember/NonMember';
+import { Member } from './Member/Member';
+function Header() {
   const [validSearch, setValidSearch] = useState(false);
   const [validMenu, setValidMenu] = useState(false);
 
@@ -26,23 +23,6 @@ function PageHeader() {
   const [validUnderMenu3, setValidUnderMenu3] = useState(false);
   const [validUnderMenu4, setValidUnderMenu4] = useState(false);
   const [validUnderMenu5, setValidUnderMenu5] = useState(false);
-  const [validToken, setValidToken] = useState(false);
-  const [openLoginModal, setOpenLoginModal] = useState(false);
-  const [validLogin, setValidLogin] = useState('');
-
-  const clickJoin = () => {
-    navigate('/join');
-  };
-  const getToken = window.localStorage.getItem('token');
-
-  // 모달창 여는 함수
-  const openLogin = () => {
-    setOpenLoginModal(true);
-  };
-  // 모달창 닫는 함수
-  const closeLogin = () => {
-    setOpenLoginModal(false);
-  };
 
   const [ValidMember, setValidMember] = useState(false);
   function showUnderMenu1() {
@@ -96,27 +76,10 @@ function PageHeader() {
     setValidMember(true);
     setValidSearch(false);
     setValidMenu(false);
-    if (getToken !== null) {
-      setValidToken(true);
-    } else {
-      setValidToken(false);
-    }
   }
-
   function memberClose() {
     setValidMember(false);
   }
-  useEffect(() => {
-    if (getToken !== null) {
-      setValidLogin('로그아웃');
-    } else {
-      setValidLogin('로그인');
-    }
-  }, []);
-  const Logout = () => {
-    localStorage.clear();
-    window.location.href = '/';
-  };
   return (
     <>
       <div className={css.headerContainer}>
@@ -128,13 +91,8 @@ function PageHeader() {
               <span>고객센터</span>
             </div>
             <div className={css.memberBar}>
-              {
-                <span onClick={validLogin == '로그인' ? openLogin : Logout}>
-                  {validLogin}
-                </span>
-              }
-              {openLoginModal && <LoginModal closeLogin={closeLogin} />}
-              <span onClick={clickJoin}>회원가입</span>
+              <span>로그인</span>
+              <span>회원가입</span>
               <span>빠른예매</span>
             </div>
           </div>
@@ -167,7 +125,6 @@ function PageHeader() {
                 </span>
               </div>
             </li>
-            {openLoginModal && <LoginModal closeLogin={closeLogin} />}
             <div className={css.mainMenuBarColumn}>
               <li className={css.mainMenuBar}>
                 <div
@@ -178,18 +135,16 @@ function PageHeader() {
                   <Link
                     style={{
                       borderBottom: validUnderMenu1
-                        ? '2px solid white'
+                        ? '4px solid white'
                         : 'none',
                     }}
-                    className={`${css.linkToWhere} ${css.menuMargin2}`}
+                    className={`${css.linkToWhere} ${css.menuMargin1}`}
                     to={'/login'}
                   >
                     영화
                   </Link>
                   {validUnderMenu1 ? (
                     <UnderMenu
-                      style={{ position: 'relative' }}
-                      margin={'1900px'}
                       menu1={'전체영화'}
                       menu2={'큐레이션'}
                       menu3={'무비포스트'}
@@ -200,17 +155,13 @@ function PageHeader() {
                 </div>
               </li>
               <li className={css.mainMenuBar}>
-                <div
-                  className={css.save}
-                  onMouseOver={showUnderMenu2}
-                  onMouseOut={closeUnderMenu2}
-                >
+                <div onMouseOver={showUnderMenu2} onMouseOut={closeUnderMenu2}>
                   <Link
-                    className={`${css.linkToWhere} ${css.menuMargin2}`}
-                    to={'/booking'}
+                    className={`${css.linkToWhere} ${css.menuMargin1}`}
+                    to={'/login'}
                     style={{
                       borderBottom: validUnderMenu2
-                        ? '2px solid white'
+                        ? '3.5px solid white'
                         : 'none',
                     }}
                   >
@@ -218,8 +169,7 @@ function PageHeader() {
                   </Link>
                   {validUnderMenu2 ? (
                     <UnderMenu
-                      style={{ position: 'relative' }}
-                      margin={'1900px'}
+                      margin={'430px'}
                       menu1={'빠른예매'}
                       menu2={'상영시간표'}
                       menu3={'더 부티크 프라이빗 예매'}
@@ -230,17 +180,13 @@ function PageHeader() {
                 </div>
               </li>
               <li className={css.mainMenuBar}>
-                <div
-                  className={css.save}
-                  onMouseOver={showUnderMenu3}
-                  onMouseOut={closeUnderMenu3}
-                >
+                <div onMouseOver={showUnderMenu3} onMouseOut={closeUnderMenu3}>
                   <Link
                     className={`${css.linkToWhere} ${css.menuMargin1}  ${css.menuMargin3}`}
                     to={'/login'}
                     style={{
                       borderBottom: validUnderMenu3
-                        ? '2px solid white'
+                        ? '3.5px solid white'
                         : 'none',
                     }}
                   >
@@ -248,8 +194,7 @@ function PageHeader() {
                   </Link>
                   {validUnderMenu3 ? (
                     <UnderMenu
-                      style={{ position: 'relative' }}
-                      margin={'1960px'}
+                      margin={'620px'}
                       menu1={'전체극장'}
                       menu2={'특별관'}
                       menu3={''}
@@ -266,23 +211,19 @@ function PageHeader() {
                   className={`${css.linkToWhere} ${css.menuMargin2}`}
                   to={'/'}
                 >
-                  <img src="https://ifh.cc/g/nNRb5Z.jpg"></img>
+                  <img src={mainImg}></img>
                 </Link>
               </li>
             </div>
             <div className={css.mainMenuBarColumn}>
               <li className={css.mainMenuBar}>
-                <div
-                  className={css.save}
-                  onMouseOver={showUnderMenu4}
-                  onMouseOut={closeUnderMenu4}
-                >
+                <div onMouseOver={showUnderMenu4} onMouseOut={closeUnderMenu4}>
                   <Link
                     className={`${css.linkToWhere} ${css.menuMargin2}`}
                     to={'/login'}
                     style={{
                       borderBottom: validUnderMenu4
-                        ? '2px solid white'
+                        ? '3.5px solid white'
                         : 'none',
                     }}
                   >
@@ -290,7 +231,7 @@ function PageHeader() {
                   </Link>
                   {validUnderMenu4 ? (
                     <UnderMenu
-                      margin={'1900px'}
+                      margin={'1000px'}
                       menu1={'진행중 이벤트'}
                       menu2={'지난 이벤트'}
                       menu3={'당첨자발표'}
@@ -311,17 +252,13 @@ function PageHeader() {
                 </Link>
               </li>
               <li className={css.mainMenuBar}>
-                <div
-                  className={css.save}
-                  onMouseOver={showUnderMenu5}
-                  onMouseOut={closeUnderMenu5}
-                >
+                <div onMouseOver={showUnderMenu5} onMouseOut={closeUnderMenu5}>
                   <Link
                     className={`${css.linkToWhere} ${css.menuMargin2}`}
                     to={'/login'}
                     style={{
                       borderBottom: validUnderMenu5
-                        ? '2px solid white'
+                        ? '3.5px solid white'
                         : 'none',
                     }}
                   >
@@ -329,8 +266,7 @@ function PageHeader() {
                   </Link>
                   {validUnderMenu5 ? (
                     <UnderMenu
-                      style={{ position: 'relative' }}
-                      margin={'1900px'}
+                      margin={'1250px'}
                       menu1={'메가박스 멤버십'}
                       menu2={'제휴/할인'}
                       menu3={''}
@@ -342,7 +278,7 @@ function PageHeader() {
               </li>
             </div>
             <li className={css.mainMenuBar}>
-              <Link className={`${css.linkToWhere} `} to={'/timetable'}>
+              <Link className={`${css.linkToWhere} `} to={'/login'}>
                 <FaCalendarAlt className={css.headerIcon} />
               </Link>
               <span className={`${css.linkToWhere}`} to={'/login'}>
@@ -369,11 +305,11 @@ function PageHeader() {
       <div className={css.position}>
         {validMenu ? <Dropdown style={{ position: 'relative' }} /> : ''}
         {validSearch ? <GlassDropdown style={{ position: 'relative' }} /> : ''}
-        {ValidMember && validToken ? <Member /> : ''}
-        {ValidMember && !validToken ? <NonMember /> : ''}
+        {ValidMember ? <NonMember /> : ''}
+        {/* <Member /> */}
       </div>
     </>
   );
 }
 
-export default PageHeader;
+export default Header;
